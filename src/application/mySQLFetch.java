bbd09dae753444b66638b1461545ff4a7b6e2e8c
Mike Price <m.price83@yahoo.com>
2014-11-19 16:57:08 -0600

    Created a authorized person object -- will authorize if a user is valid

--- src/application/mySQLFetch.java
+++ src/application/mySQLFetch.java
@@ -2,14 +2,16 @@ package application;
 
 import java.sql.Date;
 import java.util.ArrayList;
-
-import core.Flight;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import core.AirportLookups;
+import core.Flight;
 import database.MySQL;
 
 public class mySQLFetch {
-
+	
+	
+	//Get authorized user info
 	public boolean authorizeUser(String username, String password){
 		boolean authorized = false;
 		String usersTable = "SELECT ID FROM users WHERE username = ? && password = ?";
@@ -27,6 +29,7 @@ public class mySQLFetch {
 	return authorized;	
 	}
 	
+	//Reset password
 	public boolean resetPassword(String username, String password){
 		boolean reset = false;
 		String resetPassword = "UPDATE users SET password = ? WHERE username = ?";
@@ -36,6 +39,7 @@ public class mySQLFetch {
 		return reset;
 	}
 	
+	//Query database by src, des, srcDate, desDate
 	public ObservableList<Flight> searchFlightDates(String srcLocation, String desLocation, Date srcDate, Date depDate){
 		String flightDataTable = "SELECT flightID,airline,flightNumber,depDate,timeDep,arrDate,timeArr,srcLocation,desLocation,price FROM flightinfo WHERE srcLocation = ? && desLocation = ?"
 								+ "&& arrDate = ? && depDate = ?";
@@ -59,31 +63,67 @@ public class mySQLFetch {
 			Flight flight = new Flight(tmp);
 			data.add(flight);
 			
-			
 		}
 		return data;
 		
 		
 	}
+
+	//Query database by src, des
 	
-	// search flights by source and destination
-	
-	public boolean searchFlightNoDates(String srcLocation, String desLocation){
-		boolean found = false;
+	public ObservableList<Flight> searchFlightNoDates(String srcLocation, String desLocation){
+		
 		String flightDataTable = "SELECT flightID FROM flightinfo WHERE srcLocation = ? && desLocation = ?";
 		Object[] arguments = {srcLocation, desLocation};
 		int [] resultType = {MySQL.INTEGER};
 		ArrayList<Object[]> table = MySQL.executeQuery(flightDataTable, arguments, resultType);
-		if(table == null){
-			return false;
+		
+		if(table == null)
+		{
+			return null;
 		}
-		if(table.size() > 0)
-			found = true;
-		return found;
+		
+		ObservableList<Flight> data = FXCollections.observableArrayList();
+		for(int i = 0; i< table.size();i++)
+		{
+			Object[] tmp;
+			
+			tmp = table.get(i);
+			Flight flight = new Flight(tmp);
+			data.add(flight);
+			
+		}
+		return data;
+		
 	}
 	
-	
-	
+	//get Airport Codes
+	public ObservableList<AirportLookups> getAirportCodes(String code)
+	{
+		
+		String airportDataTable = "SELECT ID, city, airport, state, iata FROM airport_lookups WHERE iata = ?";
+		Object [] arguments = {code};
+		int [] resultType ={MySQL.INTEGER,MySQL.STRING,MySQL.STRING,MySQL.STRING,MySQL.STRING}; 
+		ArrayList<Object[]> table = MySQL.executeQuery(airportDataTable, arguments, resultType);
+		
+		if(table == null)
+		{
+			return null;
+		}
+		
+		ObservableList<AirportLookups> data = FXCollections.observableArrayList();
+		for(int i = 0; i< table.size();i++)
+		{
+			Object[] tmp;
+			
+			tmp = table.get(i);
+			AirportLookups lookups = new AirportLookups(tmp);
+			data.add(lookups);
+			
+		}
+		
+		return data;
+	}
 	
 	
 }
