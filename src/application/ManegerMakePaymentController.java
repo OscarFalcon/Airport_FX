da66cb5dd6c864beb35719b0352ecdcbfe2873bd
TurkiAljarbooa <aljarbooa@gmail.com>
2014-11-28 02:01:41 -0800

    manager make payment manager make payment Controller

---
+++ src/application/ManegerMakePaymentController.java
@@ -0,0 +1,63 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ManegerMakePaymentController {
+
+    @FXML
+    private TextField expirationDay;
+
+    @FXML
+    private TextField creditCardNumber;
+
+    @FXML
+    private TextField cardIdenticationNumber;
+
+    @FXML
+    private TextField cardholderName;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button signout;
+
+    @FXML
+    private TextField reservationNumber;
+
+    @FXML
+    private TextField billingZipCode;
+
+    @FXML
+    private Button pay;
+
+    @FXML
+    private Button myAccount;
+
+    @FXML
+    private Button home;
+
+    @FXML
+    void Home(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MyAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SignOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Pay(ActionEvent event) {
+
+    }
+
+}
