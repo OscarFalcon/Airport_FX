2e6dafbc03c02d01e43330d5df0597be34cc434f
TurkiAljarbooa <aljarbooa@gmail.com>
2014-12-16 16:31:47 -0800

    change size of manager reset password

---
+++ src/application/ResetPasswordController.java
@@ -0,0 +1,16 @@
+package application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ResetPasswordController {
+
+    @FXML
+    private Button save;
+
+    @FXML
+    void Save(ActionEvent event) {
+
+    }
+
+}
